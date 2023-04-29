c8404a11206dc0ca7fb551046ba875cf6b035671
zyqz97 <1270607842@qq.com>
2023-04-29 11:03:25 +0800

    bash

--- bash_313/0427_7_merge_sdf_nostop.sh
+++ bash_313/0427_7_merge_sdf_nostop.sh
@@ -25,7 +25,7 @@ label_name='merge'      # merge    m2f
 
 
 
-python gp_nerf/train.py  --wandb_id  $wandb_id   --exp_name  $exp_name  --wandb_run_name  $wandb_run_name  --label_name   $label_name --ignore_index  $ignore_index  --network_type    $network_type   --config_file  configs/$dataset2.yaml   --dataset_path  /data/yuqi/Datasets/MegaNeRF/$dataset1/$dataset2/$dataset2-pixsfm    --chunk_paths   /data/yuqi/Datasets/MegaNeRF/$dataset1/${dataset2}_chunk-labels-$label_name-down4       --train_iterations   $train_iterations        --val_interval  $val_interval    --ckpt_interval   $ckpt_interval      --stop_semantic_grad  $stop_semantic_grad  --use_pano_lift  $use_pano_lift     --batch_size  $batch_size   
+python gp_nerf/train.py  --wandb_id  $wandb_id   --exp_name  $exp_name  --wandb_run_name  $wandb_run_name  --label_name   $label_name --ignore_index  $ignore_index  --network_type    $network_type   --config_file  configs/$dataset2.yaml   --dataset_path  /data/yuqi/Datasets/MegaNeRF/$dataset1/$dataset2/$dataset2-labels    --chunk_paths   /data/yuqi/Datasets/MegaNeRF/$dataset1/${dataset2}_chunk-labels-$label_name-down4       --train_iterations   $train_iterations        --val_interval  $val_interval    --ckpt_interval   $ckpt_interval      --stop_semantic_grad  $stop_semantic_grad  --use_pano_lift  $use_pano_lift     --batch_size  $batch_size   
 
 
 
