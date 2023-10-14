af83091f4690397331a8218c2fc83851ad468c22
zyqz97 <1270607842@qq.com>
2023-10-14 23:04:21 +0800

    update

---
+++ scripts/1014_3d_get_2d_label_gy_filter_far_ori.py
@@ -0,0 +1,319 @@
+####选择一个图像，往相机视角方向飞
+
+
+import click
+import os
+import numpy as np
+import cv2 as cv
+from os.path import join as pjoin
+from glob import glob
+from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Slerp
+import torch
+from gp_nerf.runner_gpnerf import Runner
+from gp_nerf.opts import get_opts_base
+from argparse import Namespace
+from tqdm import tqdm
+import cv2
+from tools.unetformer.uavid2rgb import rgb2custom, custom2rgb,remapping
+
+from PIL import Image
+from pathlib import Path
+import open3d as o3d
+import pickle
+import math
+from dji.process_dji_v8_color import euler2rotation, rad
+import xml.etree.ElementTree as ET
+from collections import Counter
+from pyntcloud import PyntCloud
+import pandas as pd
+
+
+
+
+def calculate_entropy(labels):
+    unique_labels, label_counts = np.unique(labels, return_counts=True)
+    total_labels = len(labels)
+
+    probabilities = label_counts / total_labels
+    entropy = -np.sum(probabilities * np.log2(probabilities))
+
+    return entropy
+
+
+def custom2rgb_1(mask):
+    N= mask.shape[0]
+    mask_rgb = np.zeros(shape=(N, 3), dtype=np.uint8)
+    mask_convert = mask[np.newaxis, :]
+    mask_rgb[np.all(mask_convert == 0, axis=0)] = [0, 0, 0]             # cluster       black
+    
+    mask_rgb[np.all(mask_convert == 1, axis=0)] = [128, 0, 0]           # building      red
+    mask_rgb[np.all(mask_convert == 2, axis=0)] = [192, 192, 192]       # road        grey  
+    mask_rgb[np.all(mask_convert == 3, axis=0)] = [192, 0, 192]         # car           light violet
+    mask_rgb[np.all(mask_convert == 4, axis=0)] = [0, 128, 0]           # tree          green
+    mask_rgb[np.all(mask_convert == 5, axis=0)] = [128, 128, 0]         # vegetation    dark green
+    mask_rgb[np.all(mask_convert == 6, axis=0)] = [255, 255, 0]         # human         yellow
+    mask_rgb[np.all(mask_convert == 7, axis=0)] = [135, 206, 250]       # sky           light blue
+    mask_rgb[np.all(mask_convert == 8, axis=0)] = [0, 0, 128]           # water         blue
+
+    mask_rgb[np.all(mask_convert == 9, axis=0)] = [252,230,201]          # ground       egg
+    mask_rgb[np.all(mask_convert == 10, axis=0)] = [128, 64, 128]        # mountain     dark violet
+
+    # mask_rgb = cv2.cvtColor(mask_rgb, cv2.COLOR_RGB2BGR)
+    return mask_rgb
+
+
+
+def _get_train_opts() -> Namespace:
+    parser = get_opts_base()
+    parser.add_argument('--dataset_path', type=str, default='/data/yuqi/Datasets/DJI/Yingrenshi_20230926',required=False, help='')
+    parser.add_argument('--exp_name', type=str, default='logs_357/test',required=False, help='experiment name')
+    parser.add_argument('--output_path', type=str, default='zyq/1010_3d_get2dlabel_gt_10121215',required=False, help='experiment name')
+    # parser.add_argument('--output_path', type=str, default='zyq/1010_3d_get2dlabel_gt_test',required=False, help='experiment name')
+    parser.add_argument('--metaXml_path', default='/data/yuqi/Datasets/DJI/origin/Yingrenshi_20230926_origin/terra_point_ply/metadata.xml', type=str)
+
+    
+    return parser.parse_args()
+
+
+def hello(hparams: Namespace) -> None:
+    hparams.ray_altitude_range = [-95, 54]
+    hparams.dataset_type='memory_depth_dji'
+    device = 'cpu'
+    threshold=0.015
+
+    hparams.label_name = 'm2f' # ['m2f', 'merge', 'gt']
+    runner = Runner(hparams)
+
+
+    output_path = hparams.output_path
+    if not os.path.exists(output_path):
+        Path(output_path).mkdir(parents=True)
+    if not os.path.exists(os.path.join(output_path, 'vis_gy')):
+        Path(os.path.join(output_path, 'vis_gy')).mkdir(parents=True)
+
+
+
+    # #1. txt文件
+    load_ply_path = '/data/yuqi/Datasets/DJI/origin/Yingrenshi_fine-registered.txt'
+    points_nerf = np.genfromtxt(load_ply_path, usecols=(0, 1, 2))
+    print(points_nerf.shape)
+    root = ET.parse(hparams.metaXml_path).getroot()
+    translation = np.array(root.find('SRSOrigin').text.split(',')).astype(np.float64) 
+    coordinate_info = torch.load(hparams.dataset_path + '/coordinates.pt')
+    origin_drb = coordinate_info['origin_drb'].numpy()
+    pose_scale_factor = coordinate_info['pose_scale_factor']
+    ZYQ = torch.DoubleTensor([[0, 0, -1],
+                            [0, 1, 0],
+                            [1, 0, 0]])
+    ZYQ_1 = torch.DoubleTensor([[1, 0, 0],
+                            [0, math.cos(rad(135)), math.sin(rad(135))],
+                            [0, -math.sin(rad(135)), math.cos(rad(135))]])      
+    points_nerf = np.array(points_nerf)
+    # points_nerf = points_nerf[::100]
+    print(points_nerf.shape)
+    points_nerf += translation
+    points_nerf = ZYQ.numpy() @ points_nerf.T
+    points_nerf = (ZYQ_1.numpy() @ points_nerf).T
+    points_nerf = (points_nerf - origin_drb) / pose_scale_factor
+
+    if not os.path.exists(f"{output_path}/ori_color_nerfcoor.ply"):   
+        points_color = np.genfromtxt(load_ply_path, usecols=(3, 4, 5, 6))
+        points_color = np.array(points_color)
+        print(f"color: {points_color.shape}")
+
+        cloud = PyntCloud(pd.DataFrame(
+            # same arguments that you are passing to visualize_pcl
+            data=np.hstack((points_nerf[:, :3], np.uint8(points_color))),
+            columns=["x", "y", "z", "red", "green", "blue", "label"]))
+        cloud.to_file(f"{output_path}/ori_color_nerfcoor.ply")
+
+    # # 2. ply 文件
+    # point_cloud = o3d.io.read_point_cloud("zyq/2d-3d-2d_yingrenshi_m2f/point_cloud_50.ply")
+    # points_nerf = np.asarray(point_cloud.points)
+
+
+
+    print(f"{points_nerf.shape}")
+
+
+
+
+    points_nerf = torch.from_numpy(points_nerf).to(device)
+    points_nerf = points_nerf.float()
+
+    
+
+    train_items = runner.train_items
+    print(f"len train_items: {len(train_items)}")
+    split_list=[]
+    point_label_dict = {}
+    point_label_list = []
+    for i in range(points_nerf.shape[0]):
+        point_label_list.append([])
+    for metadata_item in tqdm(train_items):
+
+        if hparams.label_name == 'gt':
+            gt_label = metadata_item.load_gt()
+        else:
+            gt_label = metadata_item.load_label().to(device)
+
+        gt_label = remapping(gt_label)
+        
+        H, W = metadata_item.H, metadata_item.W
+        camera_rotation = metadata_item.c2w[:3,:3].to(device)
+        camera_position = metadata_item.c2w[:3, 3].to(device)
+        
+        camera_matrix = torch.tensor([[metadata_item.intrinsics[0], 0, metadata_item.intrinsics[2]],
+                              [0, metadata_item.intrinsics[1], metadata_item.intrinsics[3]],
+                              [0, 0, 1]]).to(device)
+
+        # NOTE: 2. 自己写，正确
+        E2 = torch.hstack((camera_rotation, camera_position.unsqueeze(-1)))
+        E2 = torch.stack([E2[:, 0], E2[:, 1]*-1, E2[:, 2]*-1, E2[:, 3]], 1).to(device)
+        w2c = torch.inverse(torch.cat((E2, torch.tensor([[0, 0, 0, 1]]).to(device)), dim=0))
+        points_homogeneous = torch.cat((points_nerf, torch.ones((points_nerf.shape[0], 1), dtype=torch.float32).to(device)), dim=1)
+        pt_3d_trans = torch.mm(w2c, points_homogeneous.t())
+        
+        pt_2d_trans = torch.mm(camera_matrix, pt_3d_trans[:3])
+        pt_2d_trans = pt_2d_trans / pt_2d_trans[2]
+        projected_points = pt_2d_trans[:2].t()
+
+        large_int = 1e6
+        image_width, image_height = int(W), int(H)
+
+
+        # 将 NumPy 数组转换为 PyTorch 张量，并移动到 GPU 上
+        image = torch.zeros((image_height, image_width)).long().to(device)
+        image_color = torch.zeros((image_height, image_width), dtype=torch.uint8).to(device)
+
+        depth_map = large_int * torch.ones((image_height, image_width, 1), dtype=torch.uint8).to(device)
+        depth_map_expand = large_int * torch.ones((image_height, image_width, 1), dtype=torch.uint8).to(device)
+
+        # 获得落在图像上的点
+        mask_x = (projected_points[:, 0] >= 0) & (projected_points[:, 0] < image_width)
+        mask_y = (projected_points[:, 1] >= 0) & (projected_points[:, 1] < image_height)
+        mask = mask_x & mask_y
+
+        meshMask = metadata_item.load_depth_dji().float().to(device)
+        
+        
+        x = projected_points[:, 0].long()
+        y = projected_points[:, 1].long()
+        x[~mask] = 0
+        y[~mask] = 0
+        mesh_depths = meshMask[y, x]
+        mesh_depths[~mask] = -1e6
+
+        depth_z = pt_3d_trans[2]
+        mask_z = depth_z < (mesh_depths[:, 0] + threshold)
+        mask_xyz = mask & mask_z
+
+        x, y = x[mask_xyz], y[mask_xyz]
+        depth = depth_z[mask_xyz]
+        idx = mask_xyz.nonzero().squeeze(-1)
+        depth_map[y, x] = depth[:, None]
+        image[y, x] = idx
+        image_color[y, x] = gt_label[y, x]
+
+        image_color = custom2rgb(image_color.cpu().numpy())
+        
+        Image.fromarray(image_color.astype(np.uint8)).save(os.path.join(output_path, 'vis_gy', f"{metadata_item.image_path.stem}.png"))
+        
+        # image_flatten = image.flatten()
+        # gt_label_flatten = gt_label.flatten()
+        # for idx, label in zip(image_flatten, gt_label_flatten):
+        #     point_label_list[idx].append(label)
+
+        # for h in range(H):
+        #    for w in range(W):
+        #        point_label_list[int(image[h, w])].append(int(gt_label[h, w]))
+        
+        image_flatten = image.flatten()
+        gt_label_flatten = gt_label.flatten()
+        image_flatten_non_zero = image_flatten[image_flatten.nonzero()]
+        gt_label_flatten_non_zero = gt_label_flatten[image_flatten.nonzero()]
+        
+        for vote_idx, label_vote in zip(image_flatten_non_zero, gt_label_flatten_non_zero):
+               point_label_list[vote_idx].append(label_vote)
+        
+    print('write pickle')
+    # 将列表保存为二进制文件
+    with open(f'{output_path}/point_label_list_gy.pkl', 'wb') as file:
+        pickle.dump(point_label_list, file)
+        
+
+    print("done")
+
+
+
+    # loaded_data = point_label_list
+    with open(f'{output_path}/point_label_list_gy.pkl', 'rb') as file:
+        # 使用 pickle.load() 读取数据
+        loaded_data = pickle.load(file)
+
+    loaded_data = [[label for label in point if label != 0] for point in loaded_data]
+
+
+    ###3 . label_num
+    label_counts = np.array([len(point) for point in loaded_data])
+    print(f"label_counts max :{max(label_counts)}, min :{(min(label_counts))}")
+
+
+    ## 1. entropy
+    #######################  计算entropy
+    print('calculate entropy')
+    entropies = [calculate_entropy(point) for point in tqdm(loaded_data)]
+    entropies_intensity = np.array(entropies)
+    np.save(f"{output_path}/entropies.npy", entropies_intensity)
+
+    # entropies_intensity = np.load(f"{output_path}/entropies.npy")
+
+    min_entropy = min(entropies_intensity)
+    max_entropy = max(entropies_intensity)
+    normalized_intensities = (entropies_intensity - min_entropy) / (max_entropy - min_entropy) * 255
+
+
+    # cloud = PyntCloud(pd.DataFrame(
+    #     # same arguments that you are passing to visualize_pcl
+    #     data=np.hstack((points_nerf[:, :3], normalized_intensities[:, np.newaxis])),
+    #     columns=["x", "y", "z", "intensity"]))
+    # cloud.to_file(f"{output_path}/entropy_pc.ply")
+
+    ### 2. 投票峰值
+    print('calculate max label')
+
+    most_common_labels = []
+    for point in loaded_data:
+        if not point:
+            # 处理空列表的情况
+            most_common_labels.append(-1)
+        else:
+            most_common_labels.append(Counter(point).most_common(1)[0][0])
+    most_common_labels = np.array(most_common_labels)
+    max_label = remapping(most_common_labels)
+    max_label_color = custom2rgb_1(max_label)
+
+    
+
+    cloud = PyntCloud(pd.DataFrame(
+        # same arguments that you are passing to visualize_pcl
+        data=np.hstack((points_nerf[:, :3], np.uint8(max_label_color), normalized_intensities[:, np.newaxis], label_counts[:, np.newaxis])),
+        columns=["x", "y", "z", "red", "green", "blue", 'entropy', 'label_num']))
+    cloud.to_file(f"{output_path}/results.ply")
+
+
+    print('done')
+
+
+if __name__ == '__main__':
+    #meshMask = torch.zeros(912, 1368, 1)
+    #projected_points = torch.rand(2222058, 2) * 500
+
+    #x = projected_points[:, 0].long()
+    #y = projected_points[:, 1].long()
+    #mesh_depths = meshMask[x, y]
+    #print(mesh_depths.shape)
+    #import pdb; pdb.set_trace()
+
+    hello(_get_train_opts())
---
+++ scripts_from_jx/4_ply_2_depth_color_meshMask_pool.py
@@ -0,0 +1,313 @@
+import os
+import sys
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+sys.path.append(".")
+
+import logging
+import argparse
+from plyfile import PlyData
+import numpy as np
+from tqdm import tqdm
+import xml.etree.ElementTree as ET
+import cv2
+import configargparse
+from pathlib import Path
+from multiprocessing import Pool
+
+import json
+from PIL import Image
+from PIL.ExifTags import TAGS
+import exifread
+import math
+import torch
+from torchvision.utils import make_grid
+import pickle
+from plyfile import PlyData, PlyElement
+import math
+
+
+def rad(x):
+    return math.radians(x)
+
+
+# 读取PLY文件
+def read_ply_file(file_path):
+    ply_data = PlyData.read(file_path)
+
+    # 获取顶点坐标和颜色数据
+    vertices = ply_data['vertex']
+    x = vertices['x']
+    y = vertices['y']
+    z = vertices['z']
+    r = vertices['red']
+    g = vertices['green']
+    b = vertices['blue']
+
+    # 将数据组合成列表
+    data = list(zip(x, y, z, r, g, b))
+
+    return np.array(data)
+
+# Define a function to process a single sample
+def process_sample(args):
+    split, metadata_list_dir, rgb_list_dir, meshMask_dir, points_homogeneous, down_scale, thres, \
+        save_dir_imgCoverList, scalar_attribute, colors, alpha, save_dir_img, hparams = args
+    log_name = metadata_list_dir[0].split('/')[-1][:-3]
+    os.makedirs(f'{save_dir_img}/logging', exist_ok=True)
+    logging.basicConfig(filename=f'{save_dir_img}/logging/process_from_{log_name}.log', level=logging.INFO)
+    logging.info(f"Processing from: {log_name}")
+    # ... (Your existing code for processing each file)
+    for metadata_file_dir, rgb_file_dir, meshMask_file_dir in tqdm(zip(metadata_list_dir, rgb_list_dir, meshMask_dir), \
+        total=len(metadata_list_dir), desc=f"processing {split} set"):
+        # 相机的姿态信息（相机的位置和旋转矩阵）
+        metadata = torch.load(metadata_file_dir, map_location='cpu')
+        camera_rotation = metadata['c2w'][:3,:3]
+        camera_position = metadata['c2w'][:3, 3]
+        camera_matrix = np.array([[metadata['intrinsics'][0]/down_scale, 0, metadata['intrinsics'][2]/down_scale],
+                                [0, metadata['intrinsics'][1]/down_scale, metadata['intrinsics'][3]/down_scale],
+                                [0, 0, 1]])
+        # NOTE: 2. 自己写，正确
+        E2 = np.hstack((camera_rotation, camera_position[:, np.newaxis]))
+        E2 = np.stack([E2[:, 0], E2[:, 1]*-1, E2[:, 2]*-1, E2[:, 3]], 1)  # try
+        w2c = np.linalg.inv(np.concatenate((E2, [[0,0,0,1]]), 0))
+        pt_3d_trans = np.dot(w2c, points_homogeneous.T)
+        pt_2d_trans = np.dot(camera_matrix, pt_3d_trans[:3]) 
+        pt_2d_trans = pt_2d_trans / pt_2d_trans[2]
+        projected_points = (pt_2d_trans)[:2, :]
+
+        # 创建空白图像
+        large_int = 1e6
+        image_width, image_height = int(metadata['W'] / down_scale), int(metadata['H'] / down_scale)
+
+        depth_map = large_int * np.ones((image_height, image_width, 1), dtype=np.uint8)
+        depth_map_expand = large_int * np.ones((image_height, image_width, 1), dtype=np.uint8)
+
+        npdata = 0 * np.ones((image_height, image_width, 1), dtype=np.uint8)
+        image = 0 * np.ones((image_height, image_width, 3), dtype=np.uint8)
+        image_expand = 0 * np.ones((image_height, image_width, 3), dtype=np.uint8)
+
+        step = 3
+        expand = True
+        filename = os.path.basename(metadata_file_dir)
+
+        mask_x = np.logical_and(projected_points[0, :] >= 0, projected_points[0, :] <= image_width)
+        mask_y = np.logical_and(projected_points[1, :] >= 0, projected_points[1, :] <= image_height)
+        mask = np.logical_and(mask_x, mask_y)
+        # 获得深度在meshMask之内的点，用mesh深度过滤点云
+        meshMask = np.load(meshMask_file_dir)
+        for threshold, save_dir_imgCoverThres in zip(thres, save_dir_imgCoverList):
+            # 获得落在图像上的点
+            img_points = projected_points[:, mask]
+            pt_3d_trans_z = pt_3d_trans[2, mask]
+            depth_z = pt_3d_trans_z
+            sclars = scalar_attribute[mask]
+            rgbs = colors[mask, :]     
+            image_thres = np.copy(npdata)
+            image_rgb = np.copy(image)
+            depth_map_thres = np.copy(depth_map)
+            # image_expand_thres = np.copy(image_expand)
+            for x, y, depth, scalar, color in zip(img_points[0], img_points[1], depth_z, sclars, rgbs):
+                x, y = int(x), int(y)
+                thresh = meshMask[y, x] + threshold
+            
+                if depth < thresh and depth < depth_map_thres[y, x]:
+                    depth_map_thres[y, x] = depth
+                    cv2.circle(image_thres, (x, y), 2, (float(scalar)), -1)
+                    cv2.circle(image_rgb, (x, y), 2, (float(color[2]), float(color[1]), float(color[0])), -1)
+                    
+                # if expand and depth < depth_map_expand[y, x]:
+                #     depth_map_expand[max(0, y - step):min(image_height, y + step), max(0, x - step):min(image_width, x + step)] = depth
+                #     cv2.circle(image_expand_thres, (x, y), 2, (float(color[2]), float(color[1]), float(color[0])), -1)
+            
+            # depth_filter = (abs(depth_map-depth_map_expand)>(2/pose_scale_factor))
+            # depth_map2 = depth_map.copy()
+            # depth_map2[depth_filter]=large_int
+            
+            Image.fromarray(image_thres[...,0].astype(np.uint16)).save(os.path.join(save_dir_img, filename[:-3] + '.png'))
+            # cv2.imwrite(os.path.join(save_dir_imgEx, filename[:-3] + '.jpg'), image_expand)
+
+            if hparams.alpha_cover:
+                original_rgb_img = cv2.imread(rgb_file_dir)
+                new_H, new_W = original_rgb_img.shape[0]//down_scale, original_rgb_img.shape[1]//down_scale
+                original_rgb_img = cv2.resize(original_rgb_img, (new_W, new_H))
+
+                alpha_color_img = image_rgb * alpha + original_rgb_img * (1-alpha)
+                cv2.imwrite(os.path.join(save_dir_imgCoverThres, filename[:-3] + '.jpg'), alpha_color_img)
+
+
+def _get_opts():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', default='/data/jxchen/dataset/dji/longhua_ds_1011_copy',type=str, required=False)
+    parser.add_argument('--output_path', default='/data/jxchen/dataset/dji/longhua_ds_1011_copy/render_gt_ply_meshMask_m2fLabel_rightcolor',type=str, required=False)
+    parser.add_argument('--meshMask_path', default='/data/jxchen/dataset/dji/longhua_ds_1011_copy',type=str)
+    # parser.add_argument('--txt_path', default='/data/jxchen/dji/Yingrenshi/dji_labeled_segmented_ply.txt',type=str)
+    parser.add_argument('--ply_path', default='/data/jxchen/dataset/dji/longhua/gt_label_simplfied_reversed_fineReg_trans.txt', type=str)
+    parser.add_argument('--metaXml_path', default='/data/yuqi/Datasets/DJI/origin/Longhua_origin/block1/terra_obj_low/metadata.xml', type=str)
+    parser.add_argument('--down_scale', type=int, default=1, help='')
+    parser.add_argument('--alpha_cover', default=True, action='store_false')
+    parser.add_argument('--m2f_label', default=True, action='store_false')
+
+    return parser.parse_known_args()[0]
+
+
+def main(hparams):
+    down_scale = hparams.down_scale
+    dataset_path = Path(hparams.dataset_path)  # 
+    meshMask_path = Path(hparams.meshMask_path)
+
+    if hparams.ply_path.split('.')[-1]=='txt':
+        # To render rgb, we need points_rgb, poses, intrics    
+        # 读取包含点云的TXT文件, 读txt比读ply快非常多
+        txt_file = hparams.ply_path
+        if txt_file=='/data/jxchen/dji/Yingrenshi/dji_labeled_segmented_ply.txt':
+            with open(txt_file, 'r') as file:
+                data_str = file.read()
+
+            # 将字符串分割成行，并使用空格分割每行中的数据
+            lines = data_str.split('\n')
+            data_list = [line.split() for line in lines][:-1]  # 去掉空白行
+
+            [print(line,i) for i, line in enumerate(data_list) if len(line)!=6]
+            # 将数据列表转换为 NumPy 数组
+            data = np.array(data_list, dtype=float)  # 假设数据是浮点数类型
+        else:
+            data = np.loadtxt(txt_file, delimiter=' ')  # 假设数据以空格分隔
+        xyz = data[:, :3]
+        scalar_attribute = data[:, 3]
+        scalar_mapping = {
+            0: (0, 0, 255),   # 蓝色 (Blue), terrain,2
+            1: (0, 255, 0),   # 绿色 (Green), vegetation,4
+            # 2: (128, 0, 128),  # 紫色 (Purple)
+            # 3: (255, 165, 0),  # 橙色 (Orange)
+            4: (255, 255, 0),  # 黄色 (Yellow), vehicle,3
+            6: (255, 0, 0),    # 红色 (Red), building,1
+        }
+        colors = np.zeros((len(scalar_attribute), 3), dtype=np.uint8)  # 初始化颜色数组
+        for key, value in scalar_mapping.items():
+            mask = (scalar_attribute == key)
+            colors[mask] = value
+
+    elif hparams.ply_path.split('.')[-1]=='ply':
+        ply_data = PlyData.read(hparams.ply_path)
+
+        # 获取XYZ坐标和标量信息列
+        x = ply_data['vertex']['x']
+        y = ply_data['vertex']['y']
+        z = ply_data['vertex']['z']
+        red = ply_data['vertex']['red']
+        green = ply_data['vertex']['green']
+        blue = ply_data['vertex']['blue']
+
+        # 将RGB颜色信息转换为NumPy数组
+        colors = np.column_stack((red, green, blue))
+
+        # 将XYZ坐标和颜色信息合并为一个NumPy数组
+        data = np.column_stack((x, y, z, colors))
+
+        # 提取XYZ坐标和标量属性
+        xyz = data[:, :3]  # 前三列是XYZ坐标
+
+        scalar_attribute = np.zeros((len(colors), 1), dtype=np.uint8)  # 初始化颜色数组
+        # 定义标量到颜色的映射函数
+        color_mapping = {
+            (0, 0, 255): 2,   # 蓝色 (Blue), terrain,2
+            (0, 255, 0): 4,   # 绿色 (Green), vegetation,4
+            # 2: (128, 0, 128),  # 紫色 (Purple)
+            # 3: (255, 165, 0),  # 橙色 (Orange)
+            (255, 255, 0): 3,  # 黄色 (Yellow), vehicle,3
+            (255, 0, 0): 1,    # 红色 (Red), building,1
+        }
+        for key, value in color_mapping.items():
+            mask = np.all(colors == key, axis=1).reshape(-1, 1)
+            scalar_attribute[mask] = value
+        scalar_attribute = scalar_attribute.squeeze()
+    print(f'load {hparams.ply_path}')  
+
+    # to process points_rgb
+    coordinate_info = torch.load(hparams.dataset_path + '/coordinates.pt')
+    origin_drb = coordinate_info['origin_drb'].numpy()
+    pose_scale_factor = coordinate_info['pose_scale_factor']
+
+    root = ET.parse(hparams.metaXml_path).getroot()
+    translation = np.array(root.find('SRSOrigin').text.split(',')).astype(np.float)    
+
+    #######################################
+    ZYQ = torch.DoubleTensor([[0, 0, -1],
+                            [0, 1, 0],
+                            [1, 0, 0]])
+    ZYQ_1 = torch.DoubleTensor([[1, 0, 0],
+                            [0, math.cos(rad(135)), math.sin(rad(135))],
+                            [0, -math.sin(rad(135)), math.cos(rad(135))]])      
+    points_nerf = np.array(xyz)
+    points_nerf += translation
+    points_nerf = ZYQ.numpy() @ points_nerf.T
+    points_nerf = (ZYQ_1.numpy() @ points_nerf).T
+    points_nerf = (points_nerf - origin_drb) / pose_scale_factor
+    points_homogeneous = np.hstack((points_nerf, np.ones((points_nerf.shape[0], 1))))
+
+    save_dir = hparams.output_path
+    save_dir_img = os.path.join(save_dir, "img")
+    save_dir_imgEx = os.path.join(save_dir, "imgEx")
+    save_dir_imgCoverThres30 = os.path.join(save_dir, "alphaCover_thres30")
+    save_dir_imgCoverThres20 = os.path.join(save_dir, "alphaCover_thres20")
+    save_dir_imgCoverThres15 = os.path.join(save_dir, "alphaCover_thres15")
+    save_dir_imgCoverThres10 = os.path.join(save_dir, "alphaCover_thres10")
+    # save_dir_imgCoverList = [save_dir_imgCoverThres30, save_dir_imgCoverThres20, save_dir_imgCoverThres15, 
+    #                         save_dir_imgCoverThres10]
+    # thres = [0.03, 0.02, 0.015, 0.01]
+    save_dir_imgCoverList = [save_dir_imgCoverThres15]
+    thres = [0.015]
+
+    os.makedirs(save_dir, exist_ok=True)
+    os.makedirs(save_dir_img, exist_ok=True)
+    os.makedirs(save_dir_imgEx, exist_ok=True)
+    os.makedirs(save_dir_imgCoverThres30, exist_ok=True)
+    os.makedirs(save_dir_imgCoverThres20, exist_ok=True)
+    os.makedirs(save_dir_imgCoverThres10, exist_ok=True)
+    os.makedirs(save_dir_imgCoverThres15, exist_ok=True)
+    
+    alpha = 0.5
+    for split in ("val", "train"):
+        metadata_dir = dataset_path / split / 'metadata'
+        metadata_list_dir = [os.path.join(metadata_dir, x) for x in os.listdir(metadata_dir)]
+        metadata_list_dir = sorted(metadata_list_dir, key=lambda x: x[-9:-3])
+        rgb_dir = dataset_path / split / 'rgbs'
+        rgb_list_dir = [os.path.join(rgb_dir, x) for x in os.listdir(rgb_dir)]
+        rgb_list_dir = sorted(rgb_list_dir, key=lambda x: x[-9:-3])
+        meshMask_dir = meshMask_path / split/ 'depth_mesh'
+        meshMask_dir = [os.path.join(meshMask_dir, x) for x in os.listdir(meshMask_dir)]
+        meshMask_dir = sorted(meshMask_dir, key=lambda x: x[-10:-4])
+
+        # Divide dataset into num_parts equal parts
+        num_parts = 16
+        subset_size = len(metadata_list_dir) // num_parts
+        if subset_size == 0:
+            num_parts = len(metadata_list_dir) - 1
+            subset_size = len(metadata_list_dir) // num_parts
+        # Create a list of arguments for parallel processing
+        args_list = []
+        for i in range(num_parts):
+            start_idx = i * subset_size
+            end_idx = (i + 1) * subset_size if i < num_parts - 1 else len(metadata_list_dir)
+            subset_metadata_list_dir = metadata_list_dir[start_idx:end_idx]
+            subset_rgb_list_dir = rgb_list_dir[start_idx:end_idx]
+            subset_meshMask_dir = meshMask_dir[start_idx:end_idx]
+            args_list.append((split, subset_metadata_list_dir, subset_rgb_list_dir, subset_meshMask_dir, points_homogeneous, \
+                down_scale, thres, save_dir_imgCoverList, scalar_attribute, colors, alpha, save_dir_img, hparams))
+
+        # Create a pool of worker processes and process the data in parallel
+        # process_sample(args_list[0])
+        num_processes = 16
+        with Pool(num_processes) as pool:
+            print("Pool is started.")
+            pool.map(process_sample, args_list)
+            print('ok')
+
+
+
+
+            
+
+if __name__ == '__main__':
+    main(_get_opts())
